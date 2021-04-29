6b46440e85d058543eaf7690cb206bad806fe4c1
ryan <4721319+rpshukla@users.noreply.github.com>
2021-04-28 20:57:37 -0600

    Save and restore cursor

--- frontend/js/Editor.js
+++ frontend/js/Editor.js
@@ -14,11 +14,26 @@ export default class Editor {
   }
 
   setText(text) {
+    // Save cursor
+    let cursor = this.editor.selection.getCursor();
+
     this.editor.setValue(text);
+
+    // Restore cursor
+    // TODO move cursor down if text changed above.
+    // TODO restore selection
     this.editor.selection.clearSelection();
+    this.editor.selection.moveCursorTo(cursor.row, cursor.column);
   }
 
   addEditListener(callback) {
     this.editor.addEventListener("input", callback);
+  }
+
+  /**
+   * Returns a object with .row and .column
+   */
+  getCursor() {
+    return this.editor.selection.getCursor();
   }
 }
